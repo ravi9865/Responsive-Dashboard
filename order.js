5e9ef599555ec6732189c430c7188325643e6be5
ravi9865 <Ranjankumarr256@gmail.com>
2023-08-14 20:50:41 +0530

    V1.0.0

---
+++ order.js
@@ -0,0 +1,48 @@
+const Orders = [{
+    productName:'Foldable Mini Drone',
+    ProductNumber:'87631',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName:'Lava Mini Drone',
+    ProductNumber:'85631',
+    paymentStatus:'Refund',
+    shipping:'Declined'
+},{
+    productName:'sumsung Mini Drone',
+    ProductNumber:'95631',
+    paymentStatus:'Paid',
+    shipping:'Deliverd'
+},
+{
+    productName:'Apple Mini Drone',
+    ProductNumber:'88631',
+    paymentStatus:'Refund',
+    shipping:'Deliverd'
+},
+{
+    productName:'Pulses Mini Drone',
+    ProductNumber:'89631',
+    paymentStatus:'Paid',
+    shipping:'Deliverd'
+},
+{
+    productName:'Foldable Mini Drone',
+    ProductNumber:'85731',
+    paymentStatus:'Refund',
+    shipping:'Deliverd'
+},
+{
+    productName:'Foldable Mini Drone',
+    ProductNumber:'85731',
+    paymentStatus:'Refund',
+    shipping:'Deliverd'
+}
+,
+{
+    productName:'Foldable Mini Drone',
+    ProductNumber:'85731',
+    paymentStatus:'Refund',
+    shipping:'Deliverd'
+}]
